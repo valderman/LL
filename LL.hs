d42a1a056fe9e35411f99200cf2a368105046e53
Josef Svenningsson <josef.svenningsson@gmail.com>
2013-03-07 18:19:40 +0100

    Abstract machine implementation.

--- LL.hs
+++ LL.hs
@@ -27,11 +27,11 @@ subst0 x = x:map var [0..]
 
 -- | Sequents                              
 data (Seq nm) = Exchange [Int] (Seq nm) -- Permute variables
-         | Ax -- Exactly 2 vars
+         | Ax (Type nm) -- Exactly 2 vars
          | Cut nm (Type nm) Int (Seq nm) (Seq nm) -- new vars in position 0
            
-         | Cross nm nm Int (Seq nm)
-         | Par Int (Seq nm) (Seq nm) -- splits at given pos.
+         | Cross (Type nm) nm nm Int (Seq nm)
+         | Par (Type nm) Int (Seq nm) (Seq nm) -- splits at given pos.
          | Plus Int (Seq nm) (Seq nm)
          | With Bool Int (Seq nm)
            
@@ -44,16 +44,18 @@ data (Seq nm) = Exchange [Int] (Seq nm) -- Permute variables
          | TUnpack Int (Seq nm)
 
          | Offer Int (Seq nm)
-         | Demand Int (Seq nm)
+         | Demand (Type nm) Int (Seq nm)
          | Ignore Int (Seq nm)
          | Alias Int nm (Seq nm)
+
+-- Abstract Machine
 
 data Cell where
   Freed :: Cell
   Tag   :: Bool -> Cell
   New   :: Cell
   Delay :: Int -> Closure -> Cell
-  Q     :: (Type nm) -> CellRef -> Cell
+  Q     :: (Type ()) -> Cell
 
 type CellRef = Int
 
@@ -67,19 +69,71 @@ type Heap = [Cell]
 
 type System = ([Closure],Heap)
 
-runClosure :: Heap -> Closure -> Maybe (Heap,Closure)
+runClosure :: Heap -> Closure -> Maybe (Heap,[Closure])
 runClosure h (Plus v a b,e,te)
   | Tag c <- h!!(e!!v) = Just (replace (e!!v) Freed h,
-                             (if c then a else b,increment v e,te))
-runClosure h (Cross _ _ v a,e,te)
-  = Just (h,(a,el++[x,x+sizeOf te (error "Game Over!")] ++ er,te))
+                             [(if c then a else b,increment v e,te)])
+runClosure h (Cross ty _ _ v a,e,te)
+  = Just (h,[(a,el++[x,x+sizeOf te ty] ++ er,te)])
   where (el,x:er) = splitAt v e
+runClosure h (Par ty v a b,e,te)
+  = Just (h,[(a,el++[z]++er,te)
+            ,(b,el++[z+sizeOf te ty]++er,te)])
+    where (el,z:er) = splitAt v e
+runClosure h (With t v a,e,te)
+  = Just (replace x (Tag t) h,[(a,el++er++[succ x],te)])
+    where (el,x:er) = splitAt v e
+
+runClosure h (SOne v a,e,te)
+  = Just(h,[(a,el++er,te)])
+    where (el,_:er) = splitAt v e
+runClosure h (SZero v,e,te)
+  = Just (h,[]) -- crash
+runClosure h (SBot,e,te)
+  = Just (h,[])
+
+runClosure h (TApp v ty a,e,te)
+  = Just (replace (e!!v) (Q ty) h,[(a,e,te)])
+  where (el,z:er) = splitAt v e
+runClosure h (TUnpack v a,e,te)
+  | Q ty <- h!!(e!!v) = Just (replace (e!!v) Freed h,
+                              [(a,e,ty:te)])
+
+runClosure h (Exchange π a,e,te)
+  = Just (h,[(a,map (π!!) e,te)])
+runClosure h (Ax ty,w:x:_,te)
+  = Just (copy te ty w x h,[])
+runClosure h (Cut _ ty v a b,e,te)
+  = Just (h++replicate (sizeOf te ty) New,[(a,v:ea,te),(b,v:eb,te)])
+  where (ea,eb) = splitAt v e
+        v       = length h
+
+runClosure h (Offer v a,e,te)
+  = Just (replace (e!!v) (Delay 1 (a,e,te)) h,[])
+runClosure h (Demand ty v a,e,te)
+  | (Delay n cl) <- h!!p
+  = Just (modifyRefCount (subtract 1) p h ++ replicate (sizeOf te ty) New
+         ,[(a,el++[length h]++er,te)])
+    where (el,p:er) = splitAt v e
+runClosure h (Ignore v a,e,te)
+  = Just (modifyRefCount (subtract 1) m h,[(a,el++er,te)])
+  where (el,m:er) = splitAt v e
+runClosure h (Alias v _ a,e,te)
+  = Just (modifyRefCount (+1) (e!!v) h,[(a,e!!v:e,te)])
+
+modifyRefCount f r h = replace r (Delay (f c) cl) h
+  where (Delay c cl) = h!!r
 
 replace n v h = let (l,_:r) = splitAt n h
                 in l ++ v : r
 
 increment n e = let (l,x:r) = splitAt n e
                 in l ++ succ x : r
+
+copy :: TypeEnv -> Type () -> Int -> Int -> Heap -> Heap
+copy te ty f t h = take t h ++ d ++ drop (t+size) h
+  where d = take size (drop f h)
+        size = sizeOf te ty
 
 sizeOf :: TypeEnv -> Type () -> Int
 sizeOf e (t1 :⊕: t2) = 1 + max (sizeOf e t1) (sizeOf e t2)
@@ -153,15 +207,15 @@ applyS :: Subst nm -> (Seq nm) -> (Seq nm)
 applyS f t = case t of
   (Exchange π a) -> Exchange π (s a)
   Cut w ty x a b -> Cut w (f ∙ ty) x (s a) (s b)
-  Cross w w' x a -> Cross w w' x (s a)         
-  Par x a b -> Par x (s a) (s b)
+  Cross ty w w' x a -> Cross ty w w' x (s a)         
+  Par ty x a b -> Par ty x (s a) (s b)
   Plus x a b -> Plus x (s a) (s b)
   With c x a -> With c x (s a)
   SOne x a -> SOne x (s a) 
   TApp x ty a -> TApp x (f ∙ ty) (s a)
   TUnpack x a -> TUnpack x (s' a)
   Offer x a -> Offer x (s a)
-  Demand x a -> Demand x (s a)
+  Demand ty x a -> Demand ty x (s a)
   Ignore x a -> Ignore x (s a)
   Alias x w a -> Alias x w (s a)
   a -> a
@@ -201,9 +255,9 @@ cut :: Monoid nm =>
 -- FIXME: in the absence of "What" cut can be eliminated so these recursive calls terminate. Otherwise, we have a problem.
 -- cut n w ty γ (Cut w' ty' δ a b) c = cut n w ty γ (cut γ w' ty' δ a b) c
 -- cut n w ty γ a (Cut w' ty' δ b c) = cut n w ty γ a (cut (n-γ+1) w' ty' δ b c)
-cut 2 w ty 1 Ax a = a
+cut 2 w ty 1 (Ax _) a = a
 cut n _ (ta :⊗: tb) 
-           γδ (Exchange π (Par γ a b)) (Cross w w' 0 c) = exchange (remove0 π++[length π-1..n-1]) $ cut n w ta γ 
+           γδ (Exchange π (Par _ γ a b)) (Cross _ w w' 0 c) = exchange (remove0 π++[length π-1..n-1]) $ cut n w ta γ 
                                                           a  
                                                           (exchange ([1..δ] ++ [0] ++ [δ+1..n-1]) $ cut (n-γ+1) w' tb δ b c )
    where δ = γδ - γ
@@ -212,7 +266,7 @@ cut n w (ta :⊕: tb)
 cut n w (Exists v ty) 
            γ (TApp 0 t a) (TUnpack 0 b) = cut n w (subst0 t ∙ ty) γ a (subst0 t ∙ b)
 cut n w (Bang ty) 
-           γ (Offer 0 a) (Demand 0 b) = cut n w ty γ a b
+           γ (Offer 0 a) (Demand _ 0 b) = cut n w ty γ a b
 cut n w ty γ (Offer 0 a) (Ignore 0 b) = ignore γ b
 cut n w ty γ (Offer 0 b) (Alias 0 w' a) = alias (reverse [0..γ-1]) (cut (n+γ) w ty γ (Offer 0 b) ((exchange ([1..γ] ++ [0] ++ [γ+1..n] ) $ cut (n+1) w' ty γ (Offer 0 b) a)))
   -- cut n w ty γ (Offer 0 b) (cut (n+1) w' ty γ (Offer 0 b) a)
@@ -226,8 +280,8 @@ ignore n a = Ignore 0 (ignore (n-1) a)
 alias [] a = a
 alias (x:xs) a = Alias x mempty $ alias xs a
 
-isPos Ax = True
-isPos (Exchange _ (Par _ _ _)) = True
+isPos (Ax _) = True
+isPos (Exchange _ (Par _ _ _ _)) = True
 isPos (With _ _ _) = True
 isPos (Offer _ _) = True
 isPos (TApp _ _ _) = True
@@ -236,15 +290,15 @@ isPos _ = False
 
 -- Hereditary exchange
 exchange π t = case t of
-  Ax -> Ax
-  (Cross w w' x c) -> Cross w w' (π!!x) (s' x c)
+  (Ax ty) -> (Ax ty)
+  (Cross ty w w' x c) -> Cross ty w w' (π!!x) (s' x c)
   Exchange ρ a -> exchange (map (π!!) ρ) a
   (With c x a) -> With c (π!!x) (s a) 
   (Plus x a b) -> Plus (π!!x) (s a) (s b)
   (TApp x t a) -> TApp (π!!x) t (s a)
   (TUnpack x a) -> TUnpack (π!!x) (s a)
   (Offer x a) -> Offer (π!!x) (s a)
-  (Demand x a) -> Demand (π!!x) (s a)
+  (Demand ty x a) -> Demand ty (π!!x) (s a)
   (Alias x w a) -> Alias (π!!x) w (s' x a)
   (Ignore x a) -> Ignore (π!!x) (del x a)
   (SOne x a) -> SOne (π!!x) (s a)
