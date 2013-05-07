41b15359b040a007c6afce6925c699aa7d0fac79
Nikita Frolov <nf@mkmks.org>
2013-05-07 15:10:39 +0200

    A small example in ILL translation

--- ILL.hs
+++ ILL.hs
@@ -3,34 +3,33 @@
 
 module ILL where
 
+import Pretty
 import LL
 
 -- TODO: polymorphism, exponential
 data ILL where
-  Var :: ILL
-  Lam :: Name -> Type -> ILL -> ILL
-  App :: Int -> -- ^ where to cut the context
+  Var  :: ILL
+  Lam  :: Name -> Type -> ILL -> ILL
+  App  :: Int -> -- ^ where to cut the context
          ILL -> ILL -> ILL
-
-  
+  Case :: Int -> ILL -> ILL -> ILL
+  Inl  :: ILL -> ILL
+  Inr  :: ILL -> ILL
 
 fromILL :: Int -> ILL -> Seq
 fromILL γ (Var) = Ax dum
 fromILL γ (Lam nm t body) = Cross dum " fx " nm 0 $ exchange ([1..γ-1] ++ [0]) $ fromILL (γ+1) body
 fromILL γδ (App γ f a) = Cut "a" "_a" dum γ (unCross γ $ fromILL γ f) (fromILL (γδ-γ) a)
+fromILL γ (Case x l r) = Plus "l" "r" (x+1) (fromILL γ l) (fromILL γ r)
+fromILL γ (Inl a) = With "l" True 0  (fromILL γ a)
+fromILL γ (Inr a) = With "r" False 0 (fromILL γ a)
 
 unCross γ s = Cut "x" "_x" dum γ s (Exchange [1,0,2] $ Par dum "a" "b" 1 (Ax dum) (Ax dum))
 
-{-
-unCross :: Int -> LL -> LL
-unCross x (Cross _ _ _  y s) | x == y = s
-unCross x (Cross v v' ty y s) = Cross v v' (if x < y then y + 1 else y)  (unCross (if x > y then x + 1 else x) s)
-unCross x (Plus v v' y s t) = Plus v v' (if x < y then y + 1 else y) (unCross x s) (unCross x t)
-...
-unCross x (Exchange π t) = Exchange π' (unCross (π!!x) t)
-unCross 0 (Ax _) = Exchange [0,2,1] $ Par dum "a" "b" 1 (Ax dum) (Ax dum)
-unCross 1 (Ax _) = ...
-unCross x (Cut v v' ty y s t) | x <  y = Cut v v' ty (y+1) (unCross x s) t
-                              | x >= y = Cut v v' ty y     s             (unCross (x - y) t)
-  
--}
+-- examples
+
+bool = One :⊕: One
+
+not_ill = Lam "x" bool (Case 0 (Inr Var) (Inl Var))
+
+pp_not_ill = fillTypes $ Deriv [] [("result", neg $ bool ⊸ bool)] (fromILL 0 not_ill)
