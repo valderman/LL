06ef88147059cceba1b69d70a231136176e963b2
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2013-05-15 15:54:44 +0200

    Sequent-recovery work

--- AM.hs
+++ AM.hs
@@ -5,13 +5,14 @@
 {-# LANGUAGE TypeSynonymInstances #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 
 -- | The abstract machine
 module AM where 
 
 import LL
-import Data.List (mapAccumL, elemIndex)
+import Data.List (mapAccumL, elemIndex, find)
 import Data.Graph
 import Data.Tree
 
@@ -271,18 +272,19 @@ compareClosures (s,e,te) h (s',e',te') h' = do
     assert v v'
     unify x x'
     compareHeaps (fetch x h) (fetch x' h')
-    
-    
 -}    
 
+
 -- Invariant: a blocked system will have only new cells in the
--- heap. (!except for exponentials)
-   {-     
-type Cl = Closure Int
+-- heap. (!except for exponentials and anything that can write in the heap)
+        
+-- TODO: sort the closures.
+
                 
-mkGraph :: [Cl] -> [((),Int,[Int])]
+
+mkGraph :: forall r. Eq r => [Closure r] -> [((),Int,[Int])]
 mkGraph cls = map mkOne cls'
-  where cls' :: [(Cl,Int)]
+  where cls' :: [(Closure r,Int)]
         cls' = zip cls [0..]
         mkOne (cl@(_,e,_),ix) = ((),ix,connected)
           where 
@@ -290,33 +292,33 @@ mkGraph cls = map mkOne cls'
             connected = [ix' | ((_,e',_),ix') <- cls', (_,x') <- e', (_,x) <- e, x == x']
         
 -- | From a blocked system, recover the tree-structure of closures.        
-mkTree :: [Cl] -> Tree Cl
+mkTree :: forall r. Eq r => [Closure r] -> Tree (Closure r)
 mkTree cls = fmap (cls!!) t
   where [t] = dff $ fst $ graphFromEdges' $ mkGraph cls
         -- there can be only one tree because we do not have mix.
 
 -- | Rename variables in sequents so they form a meaningful cut
--- structure.  In the output: the 1st variable communicates with the
--- parent; the others with each of the children in order.
--- For the root, we assume that the input has a dummy 1st variable.
-recoverCutStructure :: Tree Cl -> Tree Cl
+-- structure.  In the output: the last variable communicates with the
+-- parent; the others with each of the children in reverse order.
+recoverCutStructure :: Eq r => Tree (Closure r) -> Tree (Closure r)
 recoverCutStructure (Node c []) = Node c []
-recoverCutStructure (Node c@(_,_:e,_) cs) = Node c (map recoverCutStructure $ 
-                                                    zipWith placeFirst (map snd e) cs)
+recoverCutStructure (Node c@(_,e,_) cs) = 
+  Node c (map recoverCutStructure $ 
+          reverse [placeLast x c' | (_,x) <- e, Just c' <- [connectedTo x cs]])
+
+connectedTo x cs = find (\(Node (_,e,_) _) -> any ((== x) . snd) e) cs
 
 -- | Place the given reference in the 1st position in the env in the
 -- closure, and adapt the subtrees.
-placeFirst :: Int -> Tree Cl -> Tree Cl
-placeFirst x (Node (s,e,te) ts) = Node (Exchange ρ s, [e!!j | j <- ρ], te) ts
+placeLast :: Eq r => r -> Tree (Closure r) -> Tree (Closure r)
+placeLast x (Node (s,e,te) ts) = Node (Exchange ρ s, [e!!j | j <- ρ], te) ts
   where Just i = elemIndex x (map snd e)
-        ρ = i:[0..i-1]++[i+1..length e]
-  
-mkCuts :: Tree Cl -> Deriv
-mkCuts (Node (s,e,te) []) = s
-mkCuts (Node (s,_:e,te) ts) = (Cut "_" "_" dum, [])
+        ρ = [0..i-1]++[i+1..length e]++[i]
 
-sequentialize :: System h -> Seq
-sequentialize = error "todo" -- mkCuts . recoverCutStructure . mkTree
+mkCuts :: Tree (Closure r) -> Seq
+mkCuts (Node (s,e,te) []) = s
+mkCuts (Node (s,_:e,te) ts) = foldr (\(t,γ) -> Cut "_" "_" dum γ t) s $ zip (map mkCuts ts) [0..]
+
+sequentialize :: (Eq (Ref h)) => System h -> Seq
+sequentialize (cls,_heap) = mkCuts . recoverCutStructure . mkTree $ cls
       
-toInfer = meta "type cut on to recover"
--}
