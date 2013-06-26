a40b6dbb9296bdf02603077a41f027c2d866bef7
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2013-06-26 08:39:39 +0200

    some bib work

--- Driver.hs
+++ Driver.hs
@@ -3,11 +3,25 @@ module Driver (main) where
 import Cake
 import Cake.MarXup
 import Popl
+import System.IO (hPutStrLn, stderr)
+import Control.Monad (when)
+import System.Directory (doesFileExist)
+import Control.Monad.Trans (liftIO)
+
+refresh_jp_bib :: Act ()
+refresh_jp_bib = do
+  let jpbib = "../gitroot/bibtex/jp.bib"
+  e <- liftIO $ doesFileExist jpbib
+  when e $ do liftIO $ hPutStrLn stderr "refreshing bib"
+              system ["cp",jpbib,"."]
+              return ()
 
 main :: IO ()
 main = do
   outputTexMp name
-  cake empty $ pdflatexMarxup name
+  cake empty $ do 
+    refresh_jp_bib
+    pdflatexMarxup name
   
 name :: String  
 name = "paper"
